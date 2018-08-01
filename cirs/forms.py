1718db3152c160137c2a6e80f63f548dc57a5669
Sebastian Major <sebastian.major@charite.de>
2018-08-01 23:29:46 +0200

    All forms use Bootstrap now on all fields. For Django >= 1.11 widget template for radio select had to be added. Older version ignore it and are still working.

--- cirs/forms.py
+++ cirs/forms.py
@@ -21,7 +21,8 @@
 
 from django import forms
 from django.core import mail
-from django.forms import Form, ModelForm, Textarea, TextInput, RadioSelect, CharField
+from django.forms import (Form, ModelForm, Textarea, TextInput, RadioSelect, 
+                          CharField, Select, ClearableFileInput, DateInput)
 from django.forms.utils import ErrorList
 
 from .models import CriticalIncident, Comment, LabCIRSConfig
@@ -44,6 +45,14 @@ def notify_on_creation(form, subject='', excluded_user_id=None):
                            config.notification_sender_email,
                            to_list, fail_silently=False)
 
+
+class BootstrapRadioSelect(RadioSelect):
+    # Used only in Django >= 1.11 as before no templates were used for widgets.
+    # The built-in templates have problems when used with Bootstrap 4 class="form-check-input"
+    # Older versions simply ignore this attribute.
+    template_name="cirs/radio_option_bootstrap_4.html"
+
+
 class IncidentCreateForm(ModelForm):
     error_css_class = "error alert alert-danger"
 
@@ -51,13 +60,13 @@ class IncidentCreateForm(ModelForm):
         model = CriticalIncident
         fields = ['date', 'incident', 'reason', 'immediate_action',
                   'preventability', 'photo', 'public']
-        widgets = {"incident": Textarea(attrs={'cols': 80, 'rows': 5,
-                                               'class': "form-control"}),
-                   "reason": Textarea(attrs={'cols': 80, 'rows': 5,
-                                             'class': "form-control"}),
-                   "immediate_action": Textarea(attrs={'cols': 80, 'rows': 5,
-                                                       'class': "form-control"}),
-                   "public": RadioSelect()
+        widgets = {"date": DateInput(attrs={'class': "form-control col-sm-5"}),
+                   "incident": Textarea(attrs={'rows': 5, 'class': "form-control"}),
+                   "reason": Textarea(attrs={'rows': 5, 'class': "form-control"}),
+                   "immediate_action": Textarea(attrs={'rows': 5, 'class': "form-control"}),
+                   "preventability": Select(attrs={'class': "form-control col-sm-5"}),
+                   "photo": ClearableFileInput(attrs={'class': "form-control-file"}),
+                   "public": BootstrapRadioSelect(attrs={'class': "form-check-input"})
                    }
 
     def save(self):
@@ -69,16 +78,14 @@ class IncidentCreateForm(ModelForm):
 
 class IncidentSearchForm(Form):
     incident_code = CharField()
-
+    incident_code.widget.attrs.update({'class': "form-control col-sm-3"})
 
 class CommentForm(ModelForm):
     
     class Meta:
         model = Comment
         fields = ['text']
-        widgets = {"text": Textarea(attrs={'cols': 80, 'rows': 5, 
-                                           'class': "form-control"})
-        }
+        widgets = {"text": Textarea(attrs={'rows': 5, 'class': "form-control"})}
 
     def save(self):
         result = super(CommentForm, self).save()
