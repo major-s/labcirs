42d337f8d3e1f99c39401b884ebce91e94c8e4b7
Sebastian Major <sebastian.major@charite.de>
2018-12-03 10:59:33 +0100

    Changed version number. Use Djangos get_version.

--- cirs/__init__.py
+++ cirs/__init__.py
@@ -1,22 +1,6 @@
-import subprocess
+from django.utils.version import get_version
 
-VERSION = (4, 1, 1, "dev", 3)
-__AUTHOR__ = "Sebastian Major"
+VERSION = (5, 0, 0, 'rc', 1)
 
-
-def get_version():
-    if VERSION[3] == "final":
-        return "%s.%s.%s" % (VERSION[0], VERSION[1], VERSION[2])
-    elif VERSION[3] == "dev":
-        try:
-            return subprocess.check_output(["git", "describe", "--always"]).strip()
-        except:
-            if VERSION[2] == 0:
-                return "%s.%s.%s%s" % (VERSION[0], VERSION[1], VERSION[3],
-                                       VERSION[4])
-            return "%s.%s.%s.%s%s" % (VERSION[0], VERSION[1], VERSION[2],
-                                      VERSION[3], VERSION[4])
-    else:
-        return "%s.%s.%s%s" % (VERSION[0], VERSION[1], VERSION[2], VERSION[3])
-
-__version__ = get_version()
+__AUTHOR__ = "Sebastian Major" 
+__version__ = get_version(VERSION)
