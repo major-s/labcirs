00226c64dab8e85de837c49669fc8aeadffde5e8
Sebastian Major <sebastian.major@charite.de>
2024-04-30 15:54:20 +0200

    Modified version number

--- cirs/__init__.py
+++ cirs/__init__.py
@@ -1,6 +1,6 @@
 from django.utils.version import get_version
 
-VERSION = (6, 1, 1, 'final', 1)
+VERSION = (6, 1, 2, 'final', 1)
 
 __AUTHOR__ = "Sebastian Major" 
 __version__ = get_version(VERSION)
